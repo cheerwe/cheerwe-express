1568d5ef0666e36bfe40c93d966f471602b0191f
吉星 <shihong.xsh@alibaba-inc.com>
2015-09-05 21:21:42 +0800

    4.1.0
    * 增加res.renderPage方法
    * 增加res.setPageMode方法，为res.setPM的别名
    * 增加res.renderErrorf方法，参数为error
    * req.getMode方法，支持参数为,分隔的字符串

--- src/context.js
+++ src/context.js
@@ -9,30 +9,42 @@ var resContext = {
      * @param [Object] data 需要异步返回的数据
      * @param [string] msg 前台返回的消息
      */
-    sendJson: function (succ, data, msg) {
+    sendJson: function(succ, data, msg) {
         var json = helper.resJson(succ, data, msg);
         this.send(json)
     },
-    sendSucc: function (data, msg) {
+    sendSucc: function(data, msg) {
         this.sendJson(true, data, msg);
     },
-    sendFail: function (data, msg) {
+    sendFail: function(data, msg) {
         this.sendJson(false, data, msg);
     },
-    sendError: function (error) {
+    sendError: function(error) {
         this.sendJson(false, error, error.message);
     },
     /**
      * 设置PageMode
      * @param pm 需要设置的PageMode，使用覆盖的方式
      */
-    setPM: function (pm, applyIf) {
+    setPM: function(pm, applyIf) {
         this.pageMode = this.pageMode || {};
         if (applyIf) {
             Util.applyIf(this.pageMode, pm);
         } else {
             Util.apply(this.pageMode, pm);
         }
+    },
+    setPageMode: function(pm, applyIf) {
+        this.setPm(pm, applyIf);
+    },
+    /**
+     * 渲染页面
+     */
+    renderPage: function(page) {
+        helper.renderPage(this, page);
+    },
+    renderError: function(err, page) {
+        this.renderPage(page);
     }
 };
 
@@ -40,24 +52,29 @@ var reqContext = {
     /**
      * 获取参数名为ID的URL参数
      */
-    getId: function () {
+    getId: function() {
         return this.param('id');
     },
     /**
      * 根据mode指定的变量值，获取一个对象
      * @mode ['username','password']
      */
-    getMode: function (mode) {
+    getMode: function(mode) {
+        if (typeof(mode) == 'string') {
+            mode = mode.split(',');
+        }
         return helper.getReqMode(this, mode);
     },
     /**
      * 获取参数名称为condition的JSON字符串表单数据，并转换成对象
      */
-    getCondition: function () {
+    getCondition: function() {
         return helper.getReqCondition(this);
     }
-}
-module.exports = function (req, res) {
+};
+
+
+module.exports = function(req, res) {
     Util.apply(req, reqContext);
     Util.apply(res, resContext);
 };
